926252fb50164d01529e42436c0868b00dad48d2
John Leja <jleja@ngs.org>
2013-12-17 12:52:42 -0500

    Working Livefyre comment stream example using our Livefyre UAT environment and 2 sample streams for Animals and Adventure.

--- assets/global_nav.js
+++ assets/global_nav.js
@@ -1,24 +1,25 @@
 (function($) {
     // ONLY LOAD IF IS LARGER THAN IPHONE
 	if(screen.width > 767){
-	    var url = 'http://www.nationalgeographic.com/assets/tophat_json/?callback=?';
-    	$.ajax({
-    		type: 'GET',
-    		url: url,
-    		async: false,
-    		jsonpCallback: 'jsonCallback',
-    		contentType: "application/json",
-    		dataType: 'jsonp',
-    		success: function(json) {
-    		    //console.log(json);
-    			$('.global-header').html(json.global_nav);
-    			$('.global-header #navigation_mainB').remove();
-    			$('.celeb-125').remove();
-    		},
-    		error: function(e) {
-    			//console.log(e.message);
-    		}
-    	});
+        //var url = 'http://www.nationalgeographic.com/assets/tophat_json/?callback=?';
+        var url = 'http://live.wpf.test.nationalgeographic.com/assets/tophat_json/?callback=?';
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: false,
+            jsonpCallback: 'jsonCallback',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function(json) {
+                //console.log(json);
+                $('.global-header').html(json.global_nav);
+                $('.global-header #navigation_mainB').remove();
+                $('.celeb-125').remove();
+            },
+            error: function(e) {
+                //console.log(e.message);
+            }
+        });
 	}
 	
 	
