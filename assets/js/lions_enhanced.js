eac0ae3226460f65de9f34df14bf96731dc14261
jsnr <j@jao.co>
2013-12-16 17:54:06 -0700

    infopanel bug fixes

--- assets/js/lions_enhanced.js
+++ assets/js/lions_enhanced.js
@@ -653,7 +653,7 @@
                     var newPos = -$('.slideinfo', slide).width();
                     $('.slideinfo', slide).animate({'right': '-501px'}, 500, function(){ $('.slideinfo', slide).hide(); });
                     $('.info-headline', slide).animate({left: '-531px'}, 500);
-                    $('.info-plus', slide).removeClass('open');
+                    $('.buttons li.infobtn, .info-plus', slide).removeClass('open');
                     if(_gaq != undefined)_gaq.push(['_trackEvent', 'YIR', 'Caption', 'Close']);
                 } else {
                     $(this).addClass('open');
@@ -665,7 +665,7 @@
                     $('.slideinfo', slide).show();
                     $('.slideinfo', slide).animate({'right': '0'}, 500);
                     $('.info-headline', slide).animate({left: '-30px'}, 500);
-                    $('.info-plus', slide).addClass('open');
+                    $('.buttons li.infobtn, .info-plus', slide).addClass('open');
                     if(_gaq != undefined)_gaq.push(['_trackEvent', 'YIR', 'Caption', 'Open']);
                 }
             });
