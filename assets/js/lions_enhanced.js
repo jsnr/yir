858a8a063e9ce14cbf19d9b33da35357a7ddbb5b
jsnr <j@jao.co>
2013-12-18 08:09:07 -0700

    final fix for intro details panel behavior

--- assets/js/lions_enhanced.js
+++ assets/js/lions_enhanced.js
@@ -8,7 +8,8 @@
     // functions
     var createIndex, loadIndexFullRes, resizeIndexImages, swapIndexImages, fisherYates, createSlides, loadContent, launchFullscreen, cancelFullscreen, showShareButtons, hideShareButtons, setShareButtons, setGalleryControls, setAudioControls, showAudioCredit, hideAudioCredit, checkMuteAudio, fixGalleries, loadGalleryFullRes, galleryChange, closeOutSlide, changeLeft, changeRight, loadSlide, showSlide,  cleanUp, startContent, checkNavTimeout, killNav, startNav, startVideoLoadTimer, stopVideoLoadTimer, startPreloadAnimation, setiPadPlayBtn, resetiPadBtn, startiPadContent, setHeights, finishLoadProcess, loaderProgress;
 
-            	
+
+           	
 	function showCommentStream(hash) {
         // Remove '#/' or '#.' from the URL hash
         var loc = hash.slice(2);
