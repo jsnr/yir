2962799890f0365976456afbdab639aa58bd4d20
chmoore <chmoore@ngs.org>
2013-12-18 09:29:08 -0500

    Pulling back the headline fade

--- assets/js/lions_enhanced.js
+++ assets/js/lions_enhanced.js
@@ -1699,7 +1699,7 @@ if ($(".gallerylabel").length) {
     // HIDE NAV
     killNav = function(){
         clearInterval(navTimeoutInt);
-       $('.navbtn.leftarrow, .navbtn.rightarrow, .navbtn.index, .navbtn.about, .bottomNavBar, .audio_credit, .infotext').fadeOut();
+       $('.navbtn.leftarrow, .navbtn.rightarrow, .navbtn.index, .navbtn.about, .bottomNavBar, .audio_credit').fadeOut();
         
         navActive = false;
         
@@ -1717,7 +1717,7 @@ if ($(".gallerylabel").length) {
         navtimeout = 4;// number of seconds until 
         if($('.slide.current').attr('id') != 'entry'){
             //$('.navbtn.leftarrow, .navbtn.rightarrow, .navbtn.index, .navbtn.about, .curtitle, .bottomNavBar').fadeIn();
-            $('.navbtn.leftarrow, .navbtn.rightarrow, .navbtn.index, .navbtn.about, .bottomNavBar, .audio_credit, .infotext').fadeIn();
+            $('.navbtn.leftarrow, .navbtn.rightarrow, .navbtn.index, .navbtn.about, .bottomNavBar, .audio_credit').fadeIn();
         } else {
             $('.bottomNavBar').fadeIn();
         }
